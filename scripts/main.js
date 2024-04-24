2c1a54accf70b3a2590f684c4cd74656f58004df
dylanl24 <144718669+dylanl24@users.noreply.github.com>
2024-04-24 18:39:14 +0000

    javascript

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+console.log("Task 1")
+for (let i = 0; i < 11; i++) {
+    console.log(i*i)
+}
+
+console.log("Task 2")
+for (let i = 5; i > 0; i--) {
+    console.log(i)
+}
+console.log("Blastoff!")
+
+console.log("Task 3")
+for (let i = 0; i < 51; i+=2) {
+    console.log(i)
+}
+
+console.log("Task 4")
+let sum = 0;
+for (let i = 0; i < 101; i ++) {
+    for (let j = )
+}
