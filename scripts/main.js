f6aece641222140482c49ba387fd829d29352e6d
dylanl24 <144718669+dylanl24@users.noreply.github.com>
2024-04-26 18:25:32 +0000

    finish javasctipt

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,14 @@ for (let i = 0; i < 51; i+=2) {
 
 console.log("Task 4")
 let sum = 0;
-for (let i = 0; i < 101; i ++) {
-    for (let j = )
+for (let i = 0; i < 101; i++) {
+ sum+=i;
+}
+console.log(sum);
+
+console.log("Task 5")
+let factorial = 1;
+for (let i = 1; i <= 10; i++) {
+    factorial *= i;
+    console.log(factorial);
 }
